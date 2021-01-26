9da922a3791f8a5dcd47e3cc4bcbfe655d6a3c55
Jan Valeš <jan.vales@avast.com>
2021-01-26 19:19:26 +0100

    song analysis

--- app.js
+++ app.js
@@ -31,13 +31,14 @@ const APIController = (function() {
 
         const data = await result.json();
         console.log(data);
-
+		
+		$(".songDetails").empty();
         $(".selector").empty();
 
         for (i = 0; i < 10; i++) {
 			$(".selector").append("<div onClick='clearResults(this)' class='card item item-" + i + "'><img class='albumArt' src='" + data.tracks.items[i].album.images[0].url + "'>" +
-				                  "<p class='artist'>" + data.tracks.items[i].artists[0].name + 
-				                  "</p><p class='song'>" + data.tracks.items[i].name + "</p><i class='fas fa-arrow-right arrow-continue'></i><p class=' hidden hidden-" + i +"'>" + data.tracks.items[i].id + "</p></div><br>");
+				                  "<div class='nameInfo'><p class='artist'>" + data.tracks.items[i].artists[0].name + 
+				                  "</p><p class='song'>" + data.tracks.items[i].name + "</p></div><i class='fas fa-arrow-right arrow-continue'></i><p class=' hidden hidden-" + i +"'>" + data.tracks.items[i].id + "</p></div><br>");
 		}
 
         return data.tracks.items;
@@ -65,15 +66,62 @@ const APIController = (function() {
         console.log(dataAnalysis);
 
         const duration = await millisToMinutesAndSecond(dataAnalysis.duration_ms);
-        const keys = await getMusicKey(dataAnalysis.key, dataAnalysis.mode);
+		const keys = await getMusicKey(dataAnalysis.key, dataAnalysis.mode);
+		const date = await formatDate(new Date(dataSong.album.release_date));
+		var tempo = dataAnalysis.tempo + '';
 
         $(".songDetails").append("<div class='songInfo'><img class='albumArtDetail' src='" + dataSong.album.images[0].url + "'><h2>" + dataSong.album.artists[0].name + "</h2><br>" +
-        	"<h3>" + dataSong.name + "</h3><br><table><tr><td>Track length <b>" + duration + "</b></td><td>Key <b>" + keys[0] + "</b></td></tr><tr><td>Camelot <b>" + keys[1] + "</b></td></tr></table>"
-        	);
+			"<h3>" + dataSong.name + "</h3><br><table><tr><td>Track length <b>" + duration + "</b></td><td>Key <b>" + keys[0] + "</b></td></tr><tr><td>Camelot <b>" + keys[1] + "</b></td>"
+			+"<td>Release Date <b>" + date + "</b></td></tr>"
+			+ "<tr><td>Tempo <b>" + tempo.split('.')[0] + " BPM</b></td>"
+			+ "<td>Album <b>" + dataSong.album.name + "</b></td></tr>"
+			+ "</table>"
+			);
+
+		var acousticness = (Math.ceil(dataAnalysis.acousticness * 100) + '').split('.')[0];
+		var danceability = (Math.ceil(dataAnalysis.danceability * 100) + '').split('.')[0];
+		var energy = (Math.ceil(dataAnalysis.energy * 100) + '').split('.')[0];
+		var instrumentalness = (Math.ceil(dataAnalysis.instrumentalness * 100) + '').split('.')[0];
+		var liveness = (Math.ceil(dataAnalysis.liveness * 100) + '').split('.')[0];
+		var loudness = dataAnalysis.loudness;
+		var speechiness = (Math.ceil(dataAnalysis.speechiness * 100) + '').split('.')[0];
+		var valence = (Math.ceil(dataAnalysis.valence * 100) + '').split('.')[0];
+			
+		$(".songDetails").append("<div class='songAnalysis'><h4>Song analysis</h4>"
+		+ "<table><tr><td>Acousticness <b>" + acousticness +"%</b></td><td>Danceability <b>" + danceability + "%</b></tr>"
+		+ "<tr><td>Energy <b>" + energy + "%</b></td><td>Instrumentalness <b>" + instrumentalness + "%</b></td></tr>"
+		+ "<tr><td>Liveness <b>" + liveness + "%</b></td><td>Loudness <b>" + loudness + " dB</b></td></tr>"
+		+ "<tr><td>Speechiness <b>" + speechiness + "%</b></td><td>Valence <b>" + valence + "%</b></td></tr></table></div>"
+		);
 
         return dataSong;
 
-    }
+	}
+	
+	const formatDate = async (date) => {
+
+		if (date !== undefined && date !== "") {
+			var myDate = new Date(date);
+			var month = [
+			  "Jan",
+			  "Feb",
+			  "Mar",
+			  "Apr",
+			  "May",
+			  "Jun",
+			  "Jul",
+			  "Aug",
+			  "Sep",
+			  "Oct",
+			  "Nov",
+			  "Dec",
+			][myDate.getMonth()];
+			var str = myDate.getDate() + " " + month + " " + myDate.getFullYear();
+			return str;
+		  }
+		  return "";
+
+	  }
 
     const millisToMinutesAndSecond = async (millis) => {
 
